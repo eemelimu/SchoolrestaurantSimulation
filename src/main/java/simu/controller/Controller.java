8cb4678a24c91e2bbcb90b69bce6dfd3b78a3b82
salopietari <pietari.salo1@icloud.com>
2023-09-25 10:08:37 +0300

    Koitettiin ajaa vanhaa simulaattoria
    Ei mitään erikoista, kokeiltiin ajaa vanhaa simulaattoria ei toimi. (ei oo mitään väliä)

--- src/main/java/simu/controller/Controller.java
+++ src/main/java/simu/controller/Controller.java
@@ -20,7 +20,7 @@ public class Controller implements IControllerForV, IControllerForM{
         moottori = new OmaMoottori(this);
         moottori.setSimulointiaika(ui.getAika());
         moottori.setViive(ui.getViive());
-        System.out.println("Simulaatio käynnistetty");
+        System.out.println("Simulaatio käynnistetty"); // testi
         ui.getVisualisointi().tyhjennaNaytto();
         ((Thread)moottori).start();
     }
--- src/main/java/testi/Simulaattori.java
+++ src/main/java/testi/Simulaattori.java
@@ -1,14 +1,17 @@
 /*package testi;
 
+import simu.controller.Controller;
+import simu.controller.IControllerForM;
 import simu.framework.Moottori;
 import simu.framework.Trace;
 import simu.framework.Trace.Level;
 import simu.model.OmaMoottori;
+import simu.view.ISimulaattoriUI;
+import simu.view.SimulaattorinGUI;
 
 public class Simulaattori { //Tekstipohjainen
 
 	public static void main(String[] args) {
-		
 		Trace.setTraceLevel(Level.INFO);
 		Moottori m = new OmaMoottori();
 		m.setSimulointiaika(10000);
