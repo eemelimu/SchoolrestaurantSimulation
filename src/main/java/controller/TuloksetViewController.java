06ba82814e730fa0aff5d5a11cd38ec6d495d955
eemelimu <ebe.22@hotmail.com>
2023-10-11 10:33:57 +0300

    UI muutoksia

--- src/main/java/controller/TuloksetViewController.java
+++ src/main/java/controller/TuloksetViewController.java
@@ -1,4 +1,19 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class TuloksetViewController {
+
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    public void closeWindow() {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+
 }
